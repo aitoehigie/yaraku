1da789d3b6176e116eee1bdf36d425957d68d9fc
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-07-18 22:18:01 +0100

    refactor!: update the Book model
    Updated the Book model with table configuration settings

--- src/app/Book.php
+++ src/app/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $table = 'books';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['title', 'author'];
+
+    protected $visible = ['id', 'title', 'author'];
+
+    protected $hidden = ['created_at', 'updated_at'];
 }
