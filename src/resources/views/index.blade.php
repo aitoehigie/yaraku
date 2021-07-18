7572af1e277214fde183465fb50df4c4b78a436d
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-07-18 23:47:15 +0100

    style: create blade templates
    Created base template file and also a template for the index method already created in the BookController.

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<p class="text-lg text-center font-bold m-5">Book List</p>
+<table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
+  <tr class="text-left border-b-2 border-gray-300">
+    <th class="px-4 py-3">Title</th>
+    <th class="px-4 py-3">Author</th>
+    <th class="px-4 py-3">Delete</th>
+  </tr>
+  
+  @foreach ($books as $book)
+  <tr class="bg-gray-100 border-b border-gray-200">
+    <td class="px-4 py-3">{{ $book->name }}</td>
+    @foreach ($book as $author)
+    <td class="px-4 py-3">{{ $author->name }}</td>
+    @endforeach
+    <td class="px-4 py-3">Delete Me</td>
+  </tr> 
+  @endforeach
+</table>
+@endsection
